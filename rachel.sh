daf9723c80f93a0d688089f27cf21afec3231e7d
jansenjk <john.jansen@noaa.gov>
2016-03-03 09:00:19 -0900

    added curly brackets

--- rachel.sh
+++ rachel.sh
@@ -1,6 +1,6 @@
 #calculate reduced stats for data files at J = 100 c/bp
 for datafile in "$@"
 do
-	echo $datafile
+	echo ${datafile} #added curly brackets
 	bash goostats -J 100 -r $datafile stats-$datafile
 done
